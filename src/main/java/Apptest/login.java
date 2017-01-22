0358e3262cca73818ed58835a09791c7751e66d9
raj8github <raj8aus@gmail.com>
2017-01-22 16:40:04 +1100

    Update login.java

--- src/main/java/Apptest/login.java
+++ src/main/java/Apptest/login.java
@@ -7,18 +7,24 @@ public class login {
 
 	public static void details(AndroidDriver driver) {
 
+		//Close the pop-up ad.
 		driver.navigate().back();
-
+		
+		//Click on Login button
 		driver.findElement(By.id("com.bt.bms:id/btnLogin")).click();
 
+		//Enter email ID
 		driver.findElement(By.id("com.bt.bms:id/edtemail")).clear();
 		driver.findElement(By.id("com.bt.bms:id/edtemail")).sendKeys("raj88syd@gmail.com");
 
+		//Enter Password
 		driver.findElement(By.id("com.bt.bms:id/edtpass")).clear();
 		driver.findElement(By.id("com.bt.bms:id/edtpass")).sendKeys("Garfieldb1");
 
+		//Click on Submit button
 		driver.findElement(By.id("com.bt.bms:id/signin")).click();
 
+		//Close the app.
 		driver.quit();
 	}
 }
