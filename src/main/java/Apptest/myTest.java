fcd6c620aacc325e2e929726c1b9c465db094aeb
raj8github <raj8aus@gmail.com>
2017-01-22 16:41:01 +1100

    Update myTest.java

--- src/main/java/Apptest/myTest.java
+++ src/main/java/Apptest/myTest.java
@@ -8,7 +8,10 @@ public class myTest {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
+		//Set the capabilities and setup a driver for the app
 		AndroidDriver driver = setDriver.init();
+		
+		//Enter login details and close the app
 		login.details(driver);
 
 	}
