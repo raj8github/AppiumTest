45aac726924ff120d800648a79d230075fb91903
raj8github <raj8aus@gmail.com>
2017-01-22 16:42:21 +1100

    Update setDriver.java

--- src/main/java/Apptest/setDriver.java
+++ src/main/java/Apptest/setDriver.java
@@ -11,15 +11,22 @@ public class setDriver {
 
 	public static AndroidDriver init() throws MalformedURLException, InterruptedException {
 
+		//Setup the app
 		File appDir = new File("./src/main/resources/");
 		File app = new File(appDir, "com.bt.bms.apk");
+		
+		//Set the desired capabilities
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("device", "Android");
 		caps.setCapability("deviceName", "Android");
 		caps.setCapability("platformName", "Android");
 		caps.setCapability("app", app);
+		
+		//Start the app
 		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+		
 		Thread.sleep(5000);
+		
 		return driver;
 	}
 }
